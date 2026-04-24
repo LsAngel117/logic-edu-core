a6f0825ca018242c49d7523d8fb7b591b0c9dd00
LsAngel117 <luisaf117@hotmail.com>
2026-04-24 11:22:56 -0500

    Initial repository

---
+++ src/main/java/com/logossystemsit/logiceducore/LogicEduCoreApplication.java
@@ -0,0 +1,13 @@
+package com.logossystemsit.logiceducore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicEduCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicEduCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logossystemsit/logiceducore/LogicEduCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logossystemsit.logiceducore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicEduCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
